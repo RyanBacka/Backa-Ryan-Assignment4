4f800358aa35cf1cdf943a9207bd69a56edb9786
RyanBacka <RKBacka@Fullsail.edu>
2014-10-22 15:29:26 -0400

    Third function and updated HTML file

--- Backa-Ryan-Assignment4.js
+++ Backa-Ryan-Assignment4.js
@@ -3,29 +3,55 @@
 //Ryan Backa
 //SDI 1410
 //Project 4
+//Problem Solving Functions
 
-var isItAPhoneNum = function(string,phoneNumBoo){
-	if (string.index(3)=== "-" && string.index(7) === "-" && string.length === 12){
+//global variables
+var myNum="412-735-2367",
+	webSite = "http://www.fullsail.edu",
+	isItTrue = false,
+	email = "RKBacka@fullsail.edu";
+
+//functions
+//function to figure out if a string is in valid phone number form
+var isItAPhoneNum = function(enterString,phoneNumBoo){
+	var phoneNumBoo=false;
+	if (enterString.charAt(3)=== "-" && enterString.charAt(7) === "-" && enterString.length === 12){
 		phoneNumBoo = true;
 	} else {
 		phoneNumBoo = false
 	};
-	
-	
+	return phoneNumBoo;
 };
 
-var isItAnEmailAddress = function(string,emailBoo){
-	if (){
-		
-	}
+//function to determine if a string is in valid email form
+var isItAnEmailAddress = function(enterString,emailBoo){
+	var lookForPeriod = 0;
+	emailBoo = false;
+	if (enterString.contains("@") === true){
+		emailBoo= enterString.endsWith(".edu")||enterString.endsWith(".com")||enterString.endsWith(".org");
+	};
+	return emailBoo;		
 };
 
-var isItAURL = function(string, URLBoo){
-	if(string.index(0,4) === "http:" || string.index(0,5) === "https:" || string.index(0,4) === "HTTP:" || string.index(0,5) === "HTTPS:"){
-		URLBoo = true;
-	} else {
-		URLBoo = false;
-	}
+//funtion to determine if a string is in URL form
+var isItAURL = function(enterString, URLBoo){
+	URLBoo=false;
+	URLBoo=enterString.contains("http:")||enterString.contains("https:");
+	
+	return URLBoo;
 };
 
 var 
+
+//main code
+//calling my phone number fuction and logging the results
+isItTrue=isItAPhoneNum(myNum, isItTrue);
+console.log(isItTrue);
+
+//calling my URL function and logging the result
+isItTrue=isItAURL(webSite, isItTrue);
+console.log(isItTrue);
+
+//calling my email function and logging the results
+isItTrue=isItAnEmailAddress(email, isItTrue);
+console.log(isItTrue);
