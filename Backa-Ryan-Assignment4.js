2948e115077947032e94ca5123355c72ab85f21c
RyanBacka <RKBacka@Fullsail.edu>
2014-10-23 12:46:51 -0400

    some debugging

--- Backa-Ryan-Assignment4.js
+++ Backa-Ryan-Assignment4.js
@@ -7,19 +7,18 @@
 
 //global variables
 var myNum="412-735-2367",
-	webSite = "http://www.fullsail.edu",
+	webSite = "htt://www.fullsail.edu",
 	isItTrue = false,
-	email = "RKBacka@fullsail.edu",
+	email = "RKBacka@fullsail.ie",
 	stringNum = "72",
 	oddNum = 5,
 	evenArray=[2,4,6,8,10,12,14,16,18],
-	assortedArray=[2,"hi",8,"hello",15, true],
+	assortedArray=[2,"hi",8,"hello",15, true, "15"],
 	totalOfArray = 0;
 
 //functions
 //function to figure out if a string is in valid phone number form
 var isItAPhoneNum = function(enterString,phoneNumBoo){
-	var phoneNumBoo=false;
 	if (enterString.charAt(3)=== "-" && enterString.charAt(7) === "-" && enterString.length === 12){
 		phoneNumBoo = true;
 	} else {
@@ -30,11 +29,15 @@ var isItAPhoneNum = function(enterString,phoneNumBoo){
 
 //function to determine if a string is in valid email form
 var isItAnEmailAddress = function(enterString,emailBoo){
-	var lookForPeriod = 0;
-	emailBoo = false;
 	if (enterString.contains("@") === true){
-		emailBoo= enterString.endsWith(".edu")||enterString.endsWith(".com")||enterString.endsWith(".org");
-	};
+		if(enterString.endsWith(".edu") === true ||enterString.endsWith(".com") === true ||enterString.endsWith(".org") === true){
+			emailBoo = true;
+		} else {
+			emailBoo = false;
+		};
+	} else {
+		emailBoo = false;	
+	}
 	return emailBoo;		
 };
 
@@ -85,12 +88,12 @@ var sumOfAllNum = function(rndArray, sumOfArray){
 isItTrue=isItAPhoneNum(myNum, isItTrue);
 console.log(isItTrue);
 
-//calling my URL function and logging the result
-isItTrue=isItAURL(webSite, isItTrue);
-console.log(isItTrue);
-
 //calling my email function and logging the results
 isItTrue=isItAnEmailAddress(email, isItTrue);
+console.log(isItTrue);
+
+//calling my URL function and logging the result
+isItTrue=isItAURL(webSite, isItTrue);
 console.log(isItTrue);
 
 //calling my string to a number function and logging the result in a mathematical equation
